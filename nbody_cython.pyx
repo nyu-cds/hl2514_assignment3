2fe4a249f470653aa32cdecff0bfb1125105e4ab
Hao Liu <hl2514@nyu.edu>
2017-03-26 16:25:39 -0400

    Assignment 7 - Add cython

--- nbody_cython.pyx
+++ nbody_cython.pyx
@@ -8,13 +8,22 @@
     Improve the running time from 96 sec to 26.5 sec.
     The relative speedup is R = 96/26.5 = 3.62
 """
+
+"""
+Add Cython in nbody_cython.pyx.
+Running time reduced to 5.81 sec
+"""
+
 from itertools import combinations
 
-
-def advance(dt, BODIES, body_name_pairs):
+cdef advance(float dt, dict BODIES, list body_name_pairs):
     '''
         advance the system one timestep
     '''
+    cdef str body1, body2
+    cdef float x1, y1, z1, m1, x2, y2, z2, m2, dx, dy, dz
+    cdef list v1, v2
+    cdef float mag, mag_1, mag_2
     for (body1, body2) in body_name_pairs:
         ([x1, y1, z1], v1, m1) = BODIES[body1]
         ([x2, y2, z2], v2, m2) = BODIES[body2]
@@ -30,7 +39,10 @@ def advance(dt, BODIES, body_name_pairs):
         v2[0] += dx * mag_1
         v2[1] += dy * mag_1
         v2[2] += dz * mag_1
-        
+    
+    cdef str body
+    cdef float vx, vy, vz, m
+    cdef list r
     for body in BODIES:
         (r, [vx, vy, vz], m) = BODIES[body]
         r[0] += dt * vx
@@ -38,40 +50,51 @@ def advance(dt, BODIES, body_name_pairs):
         r[2] += dt * vz
         #   update_rs(r, dt, vx, vy, vz)
     
-def report_energy(BODIES, body_name_pairs, e=0.0):
+cdef report_energy(dict BODIES, list body_name_pairs, float e=0.0):
     '''
         compute the energy and return it so that it can be printed
     '''
+    cdef str body1, body2
+    cdef float x1, y1, z1, m1, x2, y2, z2, m2, dx, dy, dz
+    cdef list v1, v2
+
     for (body1, body2) in body_name_pairs:
         ((x1, y1, z1), v1, m1) = BODIES[body1]
         ((x2, y2, z2), v2, m2) = BODIES[body2]
         (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)
         e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)
         
+    cdef str body
+    cdef list r
+    cdef float vx, vy, vz, m
     for body in BODIES:
         (r, [vx, vy, vz], m) = BODIES[body]
         e += m * (vx * vx + vy * vy + vz * vz) / 2.
         
     return e
 
-def offset_momentum(ref, BODIES, px=0.0, py=0.0, pz=0.0):
+cdef offset_momentum(tuple ref, dict BODIES, float px=0.0, float py=0.0, float pz=0.0):
     '''
         ref is the body in the center of the system
         offset values from this reference
     '''
+    cdef str body
+    cdef list r
+    cdef float vx, vy, vz, m
     for body in BODIES:
         (r, [vx, vy, vz], m) = BODIES[body]
         px -= vx * m
         py -= vy * m
         pz -= vz * m
-        
+    
+    cdef list v
     (r, v, m) = ref
     v[0] = px / m
     v[1] = py / m
     v[2] = pz / m
 
 
-def nbody(loops, reference, iterations, BODIES, body_name_pairs):
+cdef nbody(int loops, str reference, int iterations, dict BODIES, list body_name_pairs):
     '''
         nbody simulation
         loops - number of loops to run
@@ -81,20 +104,27 @@ def nbody(loops, reference, iterations, BODIES, body_name_pairs):
     # Set up global state
     offset_momentum(BODIES[reference], BODIES)
 
-    for _ in range(loops):
-        for _ in range(iterations):
+    cdef int i, j
+
+
+    for i in range(loops):
+        for j in range(iterations):
             advance(0.01, BODIES, body_name_pairs)
         print(report_energy(BODIES, body_name_pairs))
 
-if __name__ == '__main__':
-    PI = 3.14159265358979323
-    SOLAR_MASS = 4 * PI * PI
-    DAYS_PER_YEAR = 365.24
 
-    BODIES = {
-        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+cdef float PI, SOLAR_MASS, DAYS_PER_YEAR
+cdef dict BODIES
+cdef list body_name_pairs
 
-        'jupiter': ([4.84143144246472090e+00,
+PI= 3.14159265358979323
+SOLAR_MASS = 4 * PI * PI
+DAYS_PER_YEAR = 365.24
+print('start')
+BODIES = {
+    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+
+    'jupiter': ([4.84143144246472090e+00,
                      -1.16032004402742839e+00,
                      -1.03622044471123109e-01],
                     [1.66007664274403694e-03 * DAYS_PER_YEAR,
@@ -102,7 +132,7 @@ if __name__ == '__main__':
                      -6.90460016972063023e-05 * DAYS_PER_YEAR],
                     9.54791938424326609e-04 * SOLAR_MASS),
 
-        'saturn': ([8.34336671824457987e+00,
+    'saturn': ([8.34336671824457987e+00,
                     4.12479856412430479e+00,
                     -4.03523417114321381e-01],
                    [-2.76742510726862411e-03 * DAYS_PER_YEAR,
@@ -110,7 +140,7 @@ if __name__ == '__main__':
                     2.30417297573763929e-05 * DAYS_PER_YEAR],
                    2.85885980666130812e-04 * SOLAR_MASS),
 
-        'uranus': ([1.28943695621391310e+01,
+    'uranus': ([1.28943695621391310e+01,
                     -1.51111514016986312e+01,
                     -2.23307578892655734e-01],
                    [2.96460137564761618e-03 * DAYS_PER_YEAR,
@@ -118,7 +148,7 @@ if __name__ == '__main__':
                     -2.96589568540237556e-05 * DAYS_PER_YEAR],
                    4.36624404335156298e-05 * SOLAR_MASS),
 
-        'neptune': ([1.53796971148509165e+01,
+    'neptune': ([1.53796971148509165e+01,
                      -2.59193146099879641e+01,
                      1.79258772950371181e-01],
                     [2.68067772490389322e-03 * DAYS_PER_YEAR,
@@ -126,7 +156,12 @@ if __name__ == '__main__':
                      -9.51592254519715870e-05 * DAYS_PER_YEAR],
                     5.15138902046611451e-05 * SOLAR_MASS)}
 
-    body_name_pairs = list(combinations(BODIES, 2))
-    # pairs of body names
-    nbody(100, 'sun', 20000, BODIES, body_name_pairs)
+body_name_pairs = list(combinations(BODIES, 2))
+# pairs of body names
 
+def main():
+    nbody(100, 'sun', 20000, BODIES, body_name_pairs)
+    print('finish')
+
+if __name__ == '__main__':
+    main()
